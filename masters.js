4aec08d1236162990abea86998962085562cc59f
ridhakurfi <ridhakurfi@gmail.com>
2025-11-06 15:42:15 +0700

    6 nov

---
+++ masters.js
@@ -0,0 +1,6 @@
+const modifier = ["Cozy", "Humid", "Concrete", "Natural", "Artificial"];
+const location = ["River", "Lake", "Forest", "Mountain", "Valley"];
+
+const randomization = (raygun) => {
+  return raygun[Math.floor(Math.random() * raygun.length)];
+};
