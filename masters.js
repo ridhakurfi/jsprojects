689218358325ab4797ee086a05762f60a69a2a1e
ridhakurfi <ridhakurfi@gmail.com>
2025-11-07 19:30:09 +0700

    7

--- masters.js
+++ masters.js
@@ -1,8 +1,23 @@
 const modifier = ["Cozy", "Humid", "Concrete", "Natural", "Artificial"];
 const location = ["River", "Lake", "Forest", "Mountain", "Valley"];
 
-const randomization = (raygun) => {
-  return raygun[Math.floor(Math.random() * raygun.length)];
+const randomizer = (rayearth) => {
+  return rayearth[Math.floor(Math.random() * rayearth.length)];
 };
 
-randomization(location)
+const vacation = () => {
+  console.log(`${randomizer(modifier)} ${randomizer(location)}`);
+};
+
+for (let i = 0; i < 5; i++) {
+  vacation();
+}
+
+let number = Math.floor(Math.random() * 100); // random 0–99
+console.log("Number:", number);
+
+if (number % 2 === 0) {
+  console.log("It's even");
+} else {
+  console.log("It's odd");
+}
