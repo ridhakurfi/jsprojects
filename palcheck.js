f0c5df3db75847bbe6d4e5475d909ebdbfae9a94
ridhakurfi <ridhakurfi@gmail.com>
2025-11-10 20:00:55 +0700

    10

---
+++ palcheck.js
@@ -0,0 +1,25 @@
+function palindromeSwapper(str) {
+  let newStr = "";
+  for (let i = 0; i < str.length - 1; i++) {
+    for (let j = 0; j < str.length; j++) {
+      if (i === j) {
+        newStr += str[j++];
+        newStr += str[j];
+        j++;
+      } else {
+        newStr += str[j];
+      }
+    }
+    if (isPalindrome(newStr) || isPalindrome(str)) return true;
+    return false;
+  }
+  return false;
+}
+
+function isPalindrome(str) {
+  if (str.split("").reverse().join("") === str) return true;
+  return false;
+}
+
+const a = palindromeSwapper("korok");
+console.log(a);
