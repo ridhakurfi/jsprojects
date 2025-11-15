25bbcfe56d1c6d55376e61774183d036c60a3def
ridhakurfi <ridhakurfi@gmail.com>
2025-11-15 13:03:22 +0700

    15

--- palcheck.js
+++ palcheck.js
@@ -20,12 +20,46 @@
 //   return false;
 // }
 
-const letterCounter = (wording) => {
-  let wordingNew = {};
-  for (let i = 0; i < wording.length; i++) {
-    wordingNew[wording[i]];
-  }
-  console.log(wordingNew);
+// const letterCounter = (wording) => {
+//   let wordingNew = {};
+//   for (let i = 0; i < wording.length; i++) {
+//     wordingNew[wording[i]];
+//   }
+//   console.log(wordingNew);
+// };
+
+// letterCounter("aaabbcc");
+
+// function letterCounting(wording) {
+//   let counted = {};
+//   for (let i = 0; i < wording.length; i++) {
+//     let char = wording[i];
+//     if (counted[char] === undefined) {
+//       counted[char] = 1;
+//     } else {
+//       counted[char] = counted[char] + 1;
+//     }
+//   }
+//   return counted;
+// }
+
+// console.log(letterCounting("aabbbbccccc"));
+
+const color = ["Blue", "Red", "Green", "White", "Purple"];
+const firstName = ["Letter", "Box", "Mail", "Envelope", "Package"];
+const lastName = ["Media", "Deliveries", "Mailman", "Visuals", "Design"];
+
+const randoming = (rayray) => {
+  return rayray[Math.floor(Math.random() * rayray.length)];
 };
 
-letterCounter("aaabbcc");
+const franchiseGenerator = () => {
+  const c = randoming(color);
+  const f = randoming(firstName);
+  const l = randoming(lastName);
+  return `${c} ${f} ${l}`;
+};
+
+for (let i = 0; i < 7; i++) {
+  console.log(franchiseGenerator());
+}
