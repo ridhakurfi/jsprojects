7208a998e50a3cf58c21034e3c6cc1d8043765c3
ridhakurfi <ridhakurfi@gmail.com>
2025-11-25 09:57:27 +0700

    gg

--- classpc.js
+++ classpc.js
@@ -1,9 +1,9 @@
-const shopping = [
+const all = [
   { id: 1, product: "Soda", price: 10 },
   { id: 2, product: "Tea", price: 5 },
 ];
 
-const productsWithTax = shopping.map((item) => ({
+const productsWithTax = all.map((item) => ({
   ...item,
   tax: item.price * 0.1,
   total: item.price * 1.1,
